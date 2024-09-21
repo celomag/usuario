f1005c5b3ea61da9ca242e7cb089827fb96a74b7
celomag <celo_mag@hotmail.com>
2024-09-21 18:17:08 -0300

    Configurações no yaml, inclusão de notação @Repository.

--- src/main/java/com/magtechologies/usuario/infastructure/repository/UsuarioRepository.java
+++ src/main/java/com/magtechologies/usuario/infastructure/repository/UsuarioRepository.java
@@ -3,7 +3,9 @@ package com.magtechologies.usuario.infastructure.repository;
 import com.magtechologies.usuario.infastructure.entities.UsuarioEntity;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity , Long> {
 
     UsuarioEntity findByEmail(String email);
